f0bd27ec0bc13b2f8a1e845375b8a778487c6c1a
Jizong Liang <Liang_Jizong@comcast.com>
2020-03-22 00:14:14 -0400

    first

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	cyoa "github.com/JizongL/chooseYourAdventure"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
 	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
@@ -18,11 +20,14 @@ func main() {
 		//
 		panic(err)
 	}
-	d := json.NewDecoder(f)
-	var story cyoa.Story
 
-	if err := d.Decode(&story); err != nil {
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
 		panic(err)
 	}
+
 	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server at: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/JizongL/chooseYourAdventure
 
 go 1.14
+
+require golang.org/x/tools v0.0.0-20200321224714-0d839f3cf2ed // indirect
